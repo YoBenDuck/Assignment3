de59e7c3363a2cdac8276f6efe43abc8a6207ab7
Ben <YoBenDuck@gmail.com>
2016-06-30 08:37:29 -0700

    assignment 3 final final version

--- scriptpart2.js
+++ scriptpart2.js
@@ -1,161 +1,161 @@
 // The Rock, Paper, Scissors Game
 /*
-var result = "";
+    var result = "";
 
-// prompt user choice
+    // prompt user choice
 
-var userChoice = prompt("Do you choose rock, paper or scissors?");
+    var userChoice = prompt("Do you choose rock, paper or scissors?");
 
-//enter something other than 'rock', 'paper' or 'scissors'
+    //enter something other than 'rock', 'paper' or 'scissors'
 
-userChoice = userChoice.toLowerCase();
+    userChoice = userChoice.toLowerCase();
 
-if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
+    if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
 
-// pc choice
+    // pc choice
 
-var pcChoice = Math.random();
-console.log(pcChoice);
+    var pcChoice = Math.random();
+    console.log(pcChoice);
 
-    if (pcChoice < 0.333) {
-        pcChoice = "rock";
-    } else if (pcChoice > 0.656) {
-        pcChoice = "paper";
-    } else {
-        pcChoice = "scissors";
+        if (pcChoice < 0.333) {
+            pcChoice = "rock";
+        } else if (pcChoice > 0.656) {
+            pcChoice = "paper";
+        } else {
+            pcChoice = "scissors";
+        }
+
+        alert("Computer: " + pcChoice);
+
+    var whatIf = function (value1, value2, choice1, choice2) {
+        if (value1 === choice1 && value2 === choice2) {
+            return (true);
+        } else if (value1 === choice2 && value2 === choice1) {
+            return (true);
+        }
+        return (false);
+    };
+
+    var compare = function (choice1, choice2) {
+        if (whatIf("paper", "rock", choice1, choice2)) {
+            return ("paper wins");
+        } else if (whatIf("paper", "scissors", choice1, choice2)) {
+            return ("scissors wins");
+        } else if (whatIf("scissors", "rock", choice1, choice2)) {
+            return ("rock wins");
+        } else {
+            return ("it's a tie!");     
+        }
+    }; 
+
+        alert(compare(userChoice, pcChoice));
     }
-
-alert("Computer: " + pcChoice);
-
-var whatIf = function (value1, value2, choice1, choice2) {
-    if (value1 === choice1 && value2 === choice2) {
-        return (true);
-    } else if (value1 === choice2 && value2 === choice1) {
-        return (true);
+    else { 
+        alert("Enter 'rock', 'paper', or 'scissors' you SCHMUCK! [=");
     }
-    return (false);
-};
-
-var compare = function (choice1, choice2) {
-    if (whatIf("paper", "rock", choice1, choice2)) {
-        return ("paper wins");
-    } else if (whatIf("paper", "scissors", choice1, choice2)) {
-        return ("scissors wins");
-    } else if (whatIf("scissors", "rock", choice1, choice2)) {
-        return ("rock wins");
-    } else {
-        return ("it's a tie!");     
-    }
-}; 
-
-alert(compare(userChoice, pcChoice));
-}
-else { 
-    alert("Enter 'rock', 'paper', or 'scissors' you SCHMUCK! [=");
-}
 */
 
 //The Basic Calculator 
 /*
-var firstNumber = prompt("Enter a number");
-var secondNumber = prompt("Enter a second number");
-var operator = prompt("Would you like to do: +, -, * or /");
-    if (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/') {
-		alert('Invalid entry.');
-		}  
+    var firstNumber = prompt("Enter a number");
+    var secondNumber = prompt("Enter a second number");
+    var operator = prompt("Would you like to do: +, -, * or /");
+        if (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/') {
+            alert('Invalid entry.');
+            }  
 
-var calculate = function(firstNumber, secondNumber, operator) {
-	var a = Number(firstNumber);
-	var b = Number(secondNumber);
-	
-    var result;
+    var calculate = function(firstNumber, secondNumber, operator) {
+        var a = Number(firstNumber);
+        var b = Number(secondNumber);
+
+        var result;
     
 // *** switch (expression) ***  
-switch (operator) {
-    case "+" :
-		result = (a + b);
-		break;
-    case "-" :
-		result = (a - b);
-		break;
-    case "*" :
-		result = (a * b);
-		break;
-    case "/" :
-		result = (a / b);
-		break;
-		default :
-		result = "Invalid. Refresh page to start again";
-	}
-	alert("The answer is: " + result);
-}
-calculate(firstNumber, secondNumber, operator);
+    switch (operator) {
+        case "+" :
+            result = (a + b);
+            break;
+        case "-" :
+            result = (a - b);
+            break;
+        case "*" :
+            result = (a * b);
+            break;
+        case "/" :
+            result = (a / b);
+            break;
+            default :
+            result = "Invalid. Refresh page to start again";
+        }
+        alert("The answer is: " + result);
+    }
+    calculate(firstNumber, secondNumber, operator);
 */
 
 // Death by JavaScript 
 
 //Part1
 /*
-function alphabet_order(str) {  
-      return str.split('').sort().join('');  
-  }  
-    console.log(alphabet_order("webmaster"));  
+    function alphabet_order(str) {  
+          return str.split('').sort().join('');  
+      }  
+        console.log(alphabet_order("webmaster"));  
 */
 
 //Part2
 /*
-var capitalize = function(str) {
-	
-	str = str.split(' ');	
-	
-	for (i = 0; i < str.length; i++) {
-		str[i] = str[i].split('');
-		str[i][0] = str[i][0].toUpperCase();
-		str[i] = str[i].join('');
-	}
-	str = str.join(' ');
-	return str;
-}
-console.log(capitalize('the quick brown fox'));
+    var capitalize = function(str) {
+
+        str = str.split(' ');	
+
+        for (i = 0; i < str.length; i++) {
+            str[i] = str[i].split('');
+            str[i][0] = str[i][0].toUpperCase();
+            str[i] = str[i].join('');
+        }
+        str = str.join(' ');
+        return str;
+    }
+    console.log(capitalize('the quick brown fox'));
 */
 
 //Part3
 /*
-var str = "The quick brown fox";
-var matches = str.match(/[aeiou]/gi);
+    var str = "The quick brown fox";
+    var matches = str.match(/[aeiou]/gi);
 // *** compare statement ***
-var count = matches ? matches.length : 0;
-    console.log(count + " vowels");
+    var count = matches ? matches.length : 0;
+        console.log(count + " vowels");
 */
 
 //Part4
 /*
-function makePasswd() {
-  var passwd = '';
-  var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  for (i = 1; i < 8; i++) {
-    var c = Math.floor(Math.random()*chars.length + 1);
-    passwd += chars.charAt(c);
-  }
+    function makePasswd() {
+      var passwd = '';
+      var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      for (i = 1; i < 8; i++) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        passwd += chars.charAt(c);
+      }
 
-  return passwd;
+      return passwd;
 
-}
-console.log(makePasswd());
+    }
+    console.log(makePasswd());
 */
 
 //Part5
 /*
-var countryNames = ["Australia", "Germany", "United States of America"];
+    var countryNames = ["Australia", "Germany", "United States of America"];
 
-var lgth = 0;
-var longest;
+    var lgth = 0;
+    var longest;
 
-for(var i=0; i < countryNames.length; i++){
-    if(countryNames[i].length > lgth){
-        var lgth = countryNames[i].length;
-        longest = countryNames[i];
-    }      
-} 
-console.log(longest);
+    for(var i=0; i < countryNames.length; i++){
+        if(countryNames[i].length > lgth){
+            var lgth = countryNames[i].length;
+            longest = countryNames[i];
+        }      
+    } 
+    console.log(longest);
 */
